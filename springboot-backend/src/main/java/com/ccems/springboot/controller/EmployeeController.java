1c54cad519ccd5d972866bdf6c1c788ed975d371
Aqib Javaid <aqib.javaid@codeclan.com>
2022-09-27 15:20:43 +0100

    built get employee by id rest api

--- springboot-backend/src/main/java/com/ccems/springboot/controller/EmployeeController.java
+++ springboot-backend/src/main/java/com/ccems/springboot/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package com.ccems.springboot.controller;
 
+import com.ccems.springboot.exception.ResourceNotFoundException;
 import com.ccems.springboot.model.Employee;
 import com.ccems.springboot.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,9 +21,17 @@ public class EmployeeController { //handles http requests, and will define rest 
         return employeeRepository.findAll(); //returns list of objects i.e employees to the client
     }
 
-    //building the create employee REST API
+    //builds the create employee REST API
     @PostMapping //handles HTTP POST request
     public Employee createEmployee(@RequestBody Employee employee){ //requestbody annotation converts JSON into a java object
         return employeeRepository.save(employee); //save method returns employee object
+    }
+
+    //builds get employee by id REST API
+    @GetMapping("{id}") //gets employee by id
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable long id){ //ResponseEntity class constructs response of the REST API, also created getEmployeeById method
+        Employee employee = employeeRepository.findById(id) //using employee repository to get the employee object from the database
+                .orElseThrow(() -> new ResourceNotFoundException("Employee does not exist with id:" + id)); //exception handling
+        return ResponseEntity.ok(employee); //ok method provides status code 200
     }
 }
